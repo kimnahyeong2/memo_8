84599a4c26861f8c25e0040401c2086d6718ef2f
NaHyeong <donadio912@naver.com>
2023-06-05 10:38:09 +0900

    update main Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("1. 입력");
+            System.out.println("2. 목록 보기");
+            System.out.println("3. 수정");
+            System.out.println("4. 삭제");
+            System.out.println("5. 종료");
 
+            int answer = sc.nextInt();
+
+            if(answer == 1){ //입력
+
+            }
+            else if(answer == 2){ // 목록보기
+
+            }
+            else if(answer == 3){ // 수정
+
+            }
+            else if(answer == 4){ // 삭제
+
+            }
+            else{ // 종료
+                break;
+            }
+        }
     }
 }
